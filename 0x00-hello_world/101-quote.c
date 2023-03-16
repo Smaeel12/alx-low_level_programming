d142755affb03268fae22b0a6efb9fe3be8e1099
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-16 23:58:42 +0000

    correcting

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char *str = "\"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i = 0;
 
 	while (str[i] != '\0')
