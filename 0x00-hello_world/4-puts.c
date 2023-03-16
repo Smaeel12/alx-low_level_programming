e7e5f92bf24fef8de11c0d5f7f1bb6f5df327635
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-16 22:25:20 +0000

    finished the tasks N1,N2,N3,N4,N5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main  - print a string
+ * Return: The sum of the two parameters
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingal puzzle");
+	return (0);
+}
