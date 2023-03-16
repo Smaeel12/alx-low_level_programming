13b0d06c0220d2bab9e0e96c3a78646e29964a17
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-16 23:11:23 +0000

    add task N6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - function to print
- * return: empty
+ * Return: return the value
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("s%","with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function
+ * Return: return the answers
+ */
+int main(void)
+{
+	printf("Size of char: %ld bytes\n", sizeof(char));
+	printf("Size of int: %ld bytes\n", sizeof(int));
+	printf("Size of short: %ld bytes\n", sizeof(short));
+	printf("Size of long int: %ld bytes\n", sizeof(long));
+	printf("Size of long long int: %ld bytes\n", sizeof(long));
+	printf("Size of float: %ld bytes\n", sizeof(float));
+	return (0);
+}
