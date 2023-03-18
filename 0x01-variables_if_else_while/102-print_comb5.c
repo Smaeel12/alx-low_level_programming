efd9f5ebe8953f307726e43c079a4c31c6051fec
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-18 19:52:26 +0000

    correct task N12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,31 +10,32 @@ int main(void)
 	int Num2;
 	int Num3;
 	int Num4;
-	int x = 1;
+	int x = 0;
 
 	for (Num1 = 0 ; Num1 <= 9 ; Num1++)
 	{
 		for (Num2 = 0 ; Num2 <= 8 ; Num2++)
 		{
-			for (Num3 = 0 ; Num3 <= 9 ; Num3++)
+			for (Num3 = Num1 ; Num3 <= 9 ; Num3++)
 			{
 				for (Num4 = x ; Num4 <= 9 ; Num4++)
 				{
-				putchar(Num1 + '0');
-				putchar(Num2 + '0');
-				putchar(' ');
-				putchar(Num3 + '0');
-				putchar(Num4 + '0');
-
-
-				if (Num1 != 9 || Num2 != 8 || Num3 != 9 || Num4 != 9)
-				{
-				putchar(',');
-				putchar(' ');
+				if (Num1 != Num2 != Num3 != Num4)
+				{	
+					putchar(Num1 + '0');
+					putchar(Num2 + '0');
+					putchar(' ');
+					putchar(Num3 + '0');
+					putchar(Num4 + '0');
+						if (Num1 != 9 || Num2 != 8 || Num3 != 9 || Num4 != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
 				}
 				}
 			}
-		x++;
+		Num2 == 0 ? x = Num2 + 2 : Num2 + 1;
 		}
 	}
 	putchar('\n');
