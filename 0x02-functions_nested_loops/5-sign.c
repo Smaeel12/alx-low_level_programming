b48a9fbc95a2bbec446311fc6c01738c0db57ec4
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-22 21:43:01 +0000

    correct task N5 -, adding task N6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * print_sign - writes the character c to stdout
+ * print_sign - entry point
  * @n: The character entred
  *
  * Return: 1 and prints + if n is greater than zero
- * Return: 0 and prints 0 if n is zero
- * Return: 1 and prints - if n is less than zero
+ * and 0 and prints 0 if n is zero
+ * and 1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar(43);
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		return(1);
+		return (1);
 	}
 	else
 	{
