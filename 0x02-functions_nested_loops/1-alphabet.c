a8b3ab963ceae8ebe0145507f9fb81b9f4e65024
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-22 19:32:18 +0000

    add task N1, N2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - entry point
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+}
