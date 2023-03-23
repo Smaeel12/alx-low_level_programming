c15f78905a1f5a1687cfea290982186ace9bed1a
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-23 02:42:21 +0000

    fixing betty for task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,15 +7,16 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n < 98)
 	{
 		for (i = n; i <= 98 ; i++)
 		{
 		if (i != n)
-			putchar (' ');
-		printf ("%d",i);
+			putchar(' ');
+		printf("%d", i);
 		if (i != 98)
-			putchar (',');
+			putchar(',');
 		}
 	}
 
@@ -26,11 +27,11 @@ void print_to_98(int n)
 		for (i = n; i >= 98; i--)
 		{
 		if (i != n)
-			putchar (' ');
-		printf ("%d",i);
+			putchar(' ');
+		printf("%d", i);
 		if (i != 98)
-			putchar (',');
+			putchar(',');
 		}
 	}
-	putchar ('\n');
+	putchar('\n');
 }
