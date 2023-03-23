1bcd3b59cd7da9194029be4607bd63dd2cede006
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-23 22:13:16 +0000

    correcting task N3,N4 && adding task N5

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- * print_numbers - prints the numbers, from 0 to 9.
+ * print_most_numbers - prints the numbers, from 0 to 9.
  */
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; ++i)
 	{
 		if (i != 2 && i != 4)
