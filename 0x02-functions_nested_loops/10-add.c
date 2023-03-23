049587763d3a65a16032eb949ad902cc21e2922b
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-23 02:38:29 +0000

    adding task N10, N11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ * add - entry point
+ * @Num1: first int
+ * @Num2: second int
+ * Return: Result
+ */
+int add(int Num1, int Num2)
+{
+	return (Num1 + Num2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - entry point
+ * @n: number entred
+ */
+void print_to_98(int n)
+{
+	int i;
+	if (n < 98)
+	{
+		for (i = n; i <= 98 ; i++)
+		{
+		if (i != n)
+			putchar (' ');
+		printf ("%d",i);
+		if (i != 98)
+			putchar (',');
+		}
+	}
+
+
+
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+		if (i != n)
+			putchar (' ');
+		printf ("%d",i);
+		if (i != 98)
+			putchar (',');
+		}
+	}
+	putchar ('\n');
+}
