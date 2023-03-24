df67249621a32945a8d4837746b7a1e20616999f
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-24 02:35:28 +0000

    add task N11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prime numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long num = 612852475143;
+	long div= 2;
+	long prime;
+	long mod;
+
+	while (num != 1)
+	{
+		mod = num % div;
+		if (mod == 0)
+		{
+			num = num / div;
+			prime = div;
+		}
+		div++;
+	}
+	printf("%li\n", prime);
+	return (0);
+}
