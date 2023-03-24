1127a4c35feb594f3715ac4af6169d96a5167028
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-24 00:20:12 +0000

    add task N9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void main(void)
+{
+	int i, c1, c2, c1r, c2r;
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3) == 0 && (i % 5) != 0)
+			printf("Fizz");
+		else if ((i % 5) == 0 && (i % 3) != 0 )
+			printf("Buzz");
+		else if ((i % 3) == 0 && (i % 5) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d",i);
+		printf(" ");
+	}
+	printf("\n");
+}
