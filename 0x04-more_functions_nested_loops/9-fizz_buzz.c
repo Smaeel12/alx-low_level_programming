3bbf3d4839cf1df33365a07dd740e5369255818e
Smaeel12 <ismailboubkri2002@gmail.com>
2023-03-24 00:22:22 +0000

    fix betty task N9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,16 +7,17 @@
 void main(void)
 {
 	int i, c1, c2, c1r, c2r;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) != 0)
 			printf("Fizz");
-		else if ((i % 5) == 0 && (i % 3) != 0 )
+		else if ((i % 5) == 0 && (i % 3) != 0)
 			printf("Buzz");
 		else if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
 		else
-			printf("%d",i);
+			printf("%d", i);
 		printf(" ");
 	}
 	printf("\n");
