3ee62ed826a97694ceb36a701192022a36f93918
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-03 23:08:53 +0000

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /**
  * _strspn - function that gets the length of a prefix substring.
  * @s: string.
@@ -7,13 +8,30 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i;
+	int i, j;
 	unsigned int num = 0;
+	int notfound = strlen(accept);
 
 	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (s[i] == accept[i])
-			num++;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				num++;
+				break;
+			}
+			else
+				notfound--;
+		}
+		if (notfound == 0)
+		{
+			break;
+		}
+		else
+		{
+			notfound = strlen(accept);
+		}
 	}
 	return (num);
 }
