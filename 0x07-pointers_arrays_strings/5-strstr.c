dc5c069d4b7ba2c185b9b9d736e6c303678d8c90
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-04 02:07:47 +0000

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0;:q needle[j] != '\0'; j++)
+		{
+
+}
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "world";
+    char *t;
+
+    t = _strstr(s, f);
+    printf("%s\n", t);
+    return (0);
+}
