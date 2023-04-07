82c8c4c58bd25eaf744237479a1cf07f57bc1a0d
Smaeel12 <125469789+Smaeel12@users.noreply.github.com>
2023-04-07 03:20:36 +0000

    Correction of "7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything"

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,17 +1,19 @@
+#include <string.h>
 /**
  * checklenght - checks if two chars of a string are equal
  * @s: string to be checked
  * @start: first index
  * @end: last index
+ * Return: 1 if equal, else 0
  */
-* Return: 1 if equal, else 0
+
 int checklenght(char *s, int start, int end)
 {
 	if (start >= end)
 		return (1);
 	else if (s[start] != s[end])
 		return (0);
-	else 
+	else
 		return (checklenght(s, start + 1, end - 1));
 }
 /**
@@ -22,6 +24,6 @@ int checklenght(char *s, int start, int end)
 int is_palindrome(char *s)
 {
 	int len = strlen(s);
-	return(checklenght(s, 0, len - 1));
- 
+
+	return (checklenght(s, 0, len - 1));
 }
