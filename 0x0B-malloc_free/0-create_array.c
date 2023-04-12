f74e08a17f7d8d3dd0b2f28de6a903c7681e1d67
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-12 01:51:58 +0000

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,11 +15,12 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	arr = malloc(sizeof(char) * size + 1);
 	arr[size] = '\0';
-	if (arr == NULL)
-		return (NULL);
 	for (i = size; i >= 0; i--)
 		arr[i] = c;
-	return (arr);
+	if (arr == NULL)
+		return (NULL);
+	else
+		return (arr);
 
 
 }
--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,12 +1,28 @@
 #include <stdlib.h>
+#include <string.h>
 /**
- * _strdup - function that returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
+ * _strdup - function that returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as a parameter.
  * @str: string given as a parameter.
- * Return: NULL if str = NULL and On success returns a pointer to the duplicated string. 
- * It returns NULL if insufficient memory was available
+ * Return: NULL if str = NULL and On success returns a pointer to
+ * the duplicated string. It returns NULL if insufficient memory was available
  */
 char *_strdup(char *str)
 {
+	char *newstr;
+	int len = strlen(str);
+	int i;
 
+	newstr = malloc(sizeof(char) * len + 1);
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+	for (i = 0; i < len; i++)
+		newstr[i] = str[i];
+	if (newstr == NULL)
+		return (NULL);
+	else
+		return (newstr);
 }
