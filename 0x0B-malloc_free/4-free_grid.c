74568d74b1f40ade16c795fa6767a497d8a9748a
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-12 22:06:32 +0000

    Correction of "4. It's not bragging if you can back it up"

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
-/** 
- * free_grid - function that frees a 2 dimensional grid previously 
+/**
+ * free_grid - function that frees a 2 dimensional grid previously
   * created by your alloc_grid function.
  * @grid: previous grid.
  * @height: height of the array.
