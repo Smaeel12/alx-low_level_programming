4165f84e26484177cbe891836da5729ceb4de8d5
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-17 21:45:52 +0000

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - new type Structure
+ * @name: string name of dog
+ * @age: float age of dog
+ * @owner: string owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
