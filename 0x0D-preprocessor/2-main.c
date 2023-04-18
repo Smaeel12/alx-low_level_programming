edfd196510bfafda54595edb265d7062801e5369
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-18 15:40:20 +0000

    3. Function-like macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - entry point
  * Return: 0.
@@ -6,5 +7,5 @@
 int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (0);
+	return (EXIT_SUCCESS);
 }
