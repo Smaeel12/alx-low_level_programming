e35628a8db627d1c03380a485dd67733e9574562
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-19 13:31:45 +0000

    Correction of "0. What's my name"

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 /**
  * print_name - function that prints a name.
  * @name: name of the person
- * f - callback to a function
+ * @f:  callback to a function
  */
 void print_name(char *name, void (*f)(char *))
 {
