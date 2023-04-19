ff8dbcda74fac9e1554fca8556d977e65707d13d
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-19 13:44:41 +0000

    Correction of "0. What's my name"

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * print_name - function that prints a name.
  * @name: name of the person
@@ -6,5 +7,6 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name != NULL)
+		f(name);
 }
