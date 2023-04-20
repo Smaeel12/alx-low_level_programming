74a9ac5b68afc6e1e518ef1cb92b09486101e329
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-20 00:37:33 +0000

    Correction of "3. A goal is not always meant to be reached, it often serves simply as something to aim at"

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int num1 = 0, num2 = 0;
-	char *operator;
+	int (*operator)(int, int);
 	int calc;
 
 
@@ -24,18 +24,18 @@ int main(int argc, char *argv[])
 	num2 = atoi(argv[3]);
 	num1 = atoi(argv[1]);
 
-	operator = argv[2];
+	operator = get_op_func(argv[2]);
 	if (operator == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	else if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	calc = get_op_func(operator)(num1, num2);
+	calc = operator(num1, num2);
 	printf("%i\n", calc);
 	return (0);
 }
