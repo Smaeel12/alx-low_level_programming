b2279d8e1895d955d59dc8c0af8cb8d2bb519c64
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-27 22:14:56 +0000

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,11 +8,11 @@
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 1;
+	size_t count = 0;
 	const list_t *current;
 
 	current = h;
-	while (current->next != NULL)
+	while (current != NULL)
 	{
 		count++;
 		current = current->next;
