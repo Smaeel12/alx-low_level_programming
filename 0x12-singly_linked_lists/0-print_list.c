d943337363a058eb408a06eb989f90981f9b2466
Smaeel12 <ismailboubkri2002@gmail.com>
2023-04-27 22:12:42 +0000

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,24 +7,24 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 1;
+	size_t count = 0;
 
 	if (h == NULL)
 	{
 		return (0);
 	}
-	if (h->str != NULL)
+	if (h != NULL)
 	{
-		printf("[%i] %s\n", h->len, h->str);
-	}
-	else
-	{
-		printf("[0] (nil)\n");
-	}
-	if (h->next != NULL)
-	{
+		if (h->str != NULL)
+		{
+			printf("[%i] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
 		count++;
-		print_list(h->next);
+		count += print_list(h->next);
 	}
 	return (count);
 }
