346362dbbe79d5df5a554450d08302f281d2174c
Smaeel12 <ismailboubkri2002@gmail.com>
2023-05-02 22:19:08 +0000

    fix errors

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 #include <stdlib.h>
 /**
-* free_listint2 - function that frees a listint_t list and sets the head pointer to NULL.
+* free_listint2 - function that frees a listint_t list
+ * and sets the head pointer to NULL.
 * @head: Pointer to a pointer to the head of the linked list.
 */
 void free_listint2(listint_t **head)
