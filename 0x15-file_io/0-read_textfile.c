4cb7b23b55a15aab73d43bef180b5b9e29bd66be
Smaeel12 <ismailboubkri2002@gmail.com>
2023-05-08 20:24:46 +0000

    Correction of "0. Tread lightly, she is near"

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,32 +2,39 @@
 #include <stddef.h>
 #include <unistd.h>
 /**
- * read_textfile - function that reads a text file and prints it to the POSIX standard output.
- * filename: text file.
- * letters: letters is the number of letters it should read and print.
- * Return: the actual number of letters it could read and print 
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
+ * read_textfile - function that reads a text file and prints
+  * it to the POSIX standard output.
+ * @filename: text file.
+ * @letters: letters is the number of letters it should read and print.
+ * Return: the actual number of letters it could read and print
+  * if the file can not be opened or read, return 0
+   * if filename is NULL return 0
+    * if write fails or does not write the expected amount of bytes, return 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename != NULL)
+	int fp;
+	char buf[1024];
+	ssize_t n, wrl;
+
+	if (filename == NULL)
 		return (0);
-	int fp = open(filename, O_RDONLY);
+
+	fp = open(filename, O_RDONLY);
 	if (fp == -1)
 		return (0);
-	char buf[1024]; 
-	ssize_t n = read(fp, buf, letters);
+
+	n = read(fp, buf, letters);
 	if (n == -1)
 	{
-		close (fp);
+		close(fp);
 		return (0);
 	}
-	ssize_t wrl = write(STDOUT_FILENO, buf, n);
-	if (wrl < 1)
+
+	wrl = write(STDOUT_FILENO, buf, n);
+	if (wrl < -1)
 	{
-		close (fp);
+		close(fp);
 		return (0);
 	}
 	close(fp);
