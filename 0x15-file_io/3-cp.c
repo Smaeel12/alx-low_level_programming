5481d20537faa2a56870422723aa68495bd68051
Smaeel12 <ismailboubkri2002@gmail.com>
2023-05-09 00:39:31 +0000

    Correction of "3. cp"

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,39 +4,58 @@
 #include <unistd.h>
 #include <string.h>
 /**
+ * close_file - function that handle close errors.
+ * @fd: file descriptor.
+ */
+void close_file(int fd)
+{
+	int result = close(fd);
+
+	if (result == -1)
+	{
+		dprintf(2, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
+}
+/**
  * file_cp - function that copy a file to a file.
  * @file_from: the name of the file to copy from.
  * @file_to: the name of the file to copy to.
  */
 void file_cp(const char *file_from, char *file_to)
 {
-	int ff, ft, rclose;
+	int ff, ft;
 	char buf[1024];
 	ssize_t n, wrl;
 
 	ff = open(file_from, O_RDONLY);
-	n = read(ff, buf, 1024);
-	if (n == -1 || file_from == NULL || ff == -1)
+	if (ff == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		rclose = close(ff);
-		if (rclose == -1)
-		{
-			dprintf(2, "Error: Can't close from file %i\n", ff);
-			exit(100);
-		}
+		exit(98);
 	}
-	ft = open(file_to, O_RDWR | O_CREAT | O_TRUNC, 0664);
-	wrl = write(ft, buf, n);
-	if (ft == -1 || wrl == -1 || wrl != n)
+	ft = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (ft == -1)
 	{
-		rclose = close(ft);
-		if (rclose == -1)
+		dprintf(2, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	while ((n = read(ff, buf, 1024)) > 0)
+	{
+		wrl = write(ft, buf, n);
+		if (wrl == -1 || wrl != n)
 		{
-		dprintf(2, "Error: Can't close from file %i\n", ft);
-		exit(100);
+			dprintf(2, "Error: Can't write to %s\n", file_to);
+			exit(99);
 		}
 	}
+	if (n == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+	close_file(ff);
+	close_file(ft);
 }
 /**
  * main - program that copies the content of a file to another file.
