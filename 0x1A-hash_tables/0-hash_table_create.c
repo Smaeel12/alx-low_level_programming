6aac44326bb583e22f4a2b0e80a69448bc3b90a1
Smaeel12 <ismailboubkri2002@gmail.com>
2023-07-07 21:18:01 +0000

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - function that creates a hash table.
+ * @size: is the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_hash_table;
+
+	new_hash_table = malloc(sizeof(hash_table_t));
+	if (new_hash_table == NULL)
+	{
+		return (NULL);
+	}
+	new_hash_table->size = size;
+	new_hash_table->array = NULL;
+	return (new_hash_table);
+}
