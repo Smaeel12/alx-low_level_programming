6c8bfc6e4f333c6bbf0972c4d90e7bb553593e83
Smaeel12 <ismailboubkri2002@gmail.com>
2023-07-08 00:03:06 +0000

    3. >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,23 +11,29 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int key_idx = key_index((const unsigned char *) key, ht->size);
-	char *value_copy;
+	char *value_copy, *key_copy;
 	hash_node_t *new_node;
 
-	if (key == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
+	key_copy = strdup(key);
+
+	if (ht->array[key_idx] && strcmp(ht->array[key_idx]->key, key) != 0)
+	{
+		free(ht->array[key_idx]->value);
+		ht->array[key_idx]->value = value_copy;
+		return (1);
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
 		return (0);
 	}
-	value_copy = strdup(value);
-	if (value_copy == NULL)
-	{
-		free(new_node);
-		return (0);
-	}
-	new_node->key = (char *)key;
+	new_node->key = key_copy;
 	new_node->value = value_copy;
 	new_node->next = ht->array[key_idx];
 	ht->array[key_idx] = new_node;
