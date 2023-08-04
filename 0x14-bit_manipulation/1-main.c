04d687e5b929f98baa31f45ef503e8763ff9e458
Smaeel12 <ismailboubkri2002@gmail.com>
2023-08-04 16:29:33 +0000

    Correction of "1. 1"

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <limits.h>
 #include "main.h"
 
 /**
@@ -8,13 +9,13 @@
  */
 int main(void)
 {
-    print_binary(1024);
-    printf("\n");
     print_binary(0);
     printf("\n");
     print_binary(1);
     printf("\n");
     print_binary(98);
+    printf("\n");
+    print_binary(1024);
     printf("\n");
     print_binary((1 << 10) + 1);
     printf("\n");
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_binary - function that prints the binary representation of a number.
  * @n: the number
@@ -6,36 +7,24 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int bit = 1;
-	int exp = 0, idx = 0;
 
 	if (n == 0 || n == 1)
 		_putchar(n + '0');
+
 	else
 	{
 		while (bit * 2 <= n)
+		bit *= 2;
+		while (bit != 0)
 		{
-			exp++;
-			bit = bit * 2;
-		}
-		n -= bit;
-		_putchar('1');
-		while (exp != 0)
-		{
-			idx = exp - 1;
-			bit = 1;
-			while (idx != 0)
-			{
-				bit = bit * 2;
-				idx--;
-			}
-			if (n >= bit)
+			if (n & bit)
 			{
 				_putchar('1');
 				n -= bit;
 			}
 			else
 				_putchar('0');
-			exp--;
+			bit /= 2;
 		}
 	}
 }
