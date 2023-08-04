002057adb02bffe08c87261cee08df3ee55c9ceb
Smaeel12 <ismailboubkri2002@gmail.com>
2023-08-04 14:07:54 +0000

    Correction of "0. 0"

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,6 +12,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int bit = 1, uint = 0;
 	int len, e, idx = 0;
 
+	if (b == NULL)
+		return (0);
+
 	for (len = 0; b[len] != '\0'; len++)
 		;
 	len--;
