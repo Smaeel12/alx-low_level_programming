ad05f532c2a96a7d1588321051e6f65944e52e86
Smaeel12 <ismailboubkri2002@gmail.com>
2023-08-06 15:23:49 +0000

    Correction of "3. 11"

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (*n | (1 << index))
+	if (*n | (1 << index) && index <= sizeof(unsigned long int) * 8)
 	{
 		*n = *n | (1 << index);
 		return (1);
