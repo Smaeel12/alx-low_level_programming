e24cf105e0d746e17be03ddd92c6fdc1a091612b
Smaeel12 <ismailboubkri2002@gmail.com>
2023-08-06 18:15:57 +0000

    Correction of "5. 101"

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  * flip_bits - write a function that returns the number of bits
  * you would need to flip to get from one number to another.
@@ -7,21 +8,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int index = 0;
-	unsigned int num_flips = 0;
+	unsigned long int xor_num = n ^ m;
 	unsigned long int checker = 1;
+	unsigned int index = 0;
+	unsigned int num_flips = 0;
 
-	while (checker <= n || checker <= m)
+	while (checker <= xor_num)
 	{
 		checker = 1 << index;
-		if (checker & m || checker & n)
-		{
-			if ((checker & m) ^ (checker & n))
-				num_flips++;
-
-		}
+		if (checker & xor_num)
+			num_flips++;
 		index++;
-
 	}
 	return (num_flips);
 }
