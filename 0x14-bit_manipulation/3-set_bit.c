ebee83d89ac655feeb1ca44fb65e224a733bdb36
Smaeel12 <ismailboubkri2002@gmail.com>
2023-11-23 21:34:07 +0000

    3-set_bit.c 3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -6,7 +6,7 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (!(*n & (1 << index)))
+	if (!(*n & (1 << index)) && index <= sizeof(n) * 8)
 	{
 		*n = *n | (1 << index);
 		return (1);
