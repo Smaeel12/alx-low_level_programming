ac3a8c89306b1b2f2a1ae843a3e5044a0c4c9f5b
Smaeel12 <ismailboubkri2002@gmail.com>
2024-02-03 02:39:00 +0000

    Correction of "4.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *) key, ht->size);
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
 	hash_node_t *current;
 
 	if (ht == NULL || key == NULL)
@@ -18,10 +18,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	else if (strlen(key) == 0)
 		return (NULL);
 
+	if ((ht->array)[index] == NULL)
+		return (NULL);
+
 	current = ht->array[index];
 	while (current != NULL)
 	{
-		if (!(strcmp(current->key, key)))
+		if (strcmp(current->key, key) == 0)
 			return (current->value);
 		current = current->next;
 	}
