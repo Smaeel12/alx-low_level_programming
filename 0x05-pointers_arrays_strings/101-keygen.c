45647fc963004b2b0f56c84c0b8bd15fc2feb242
Smaeel12 <ismailboubkri2002@gmail.com>
2024-07-28 01:01:01 +0100

    add crackme

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
+
+/**
+ * main - generates a random code for 101-crackme
+ * Return: always 0.
+ */
 int main(void)
 {
-	char key;
-	int key_sum = 0;
+	int passwd[128], sum = 0, i;
 
 	srand(time(NULL));
-	while (key_sum <= 2772)
+
+	for (i = 0; i < 128; i++)
 	{
-		key = rand() % 94 + 33;
-		key_sum += key;
-		if (key_sum > 2772)
+		if (2772 - sum - 94 < 32)
 		{
-			int temp = key_sum - 2772;
-			key_sum -= key;
-			key = key - temp;
-			printf("%c", key);
+			passwd[i] = 2772 - sum - 94;
+			sum += passwd[i] + 94;
+			putchar(passwd[i] + 94);
 			break;
 		}
-		printf("%c", key);
+		passwd[i] = rand() % 32;
+		sum += passwd[i] + 94;
+		putchar(passwd[i] + 94);
 	}
-	printf("%i\n", key_sum);
+	/* printf("\n%i\n", sum); */
 	return (0);
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,1 +1,3 @@
-# 0x05. C - Pointers, arrays and string
+# Low-Level Programming 
+
+This repository contains programs written for the low-level programming at the alx-SoftwareEngineering program. I am learning about data structures, low-levelprogramming concepts while working in the C language.
